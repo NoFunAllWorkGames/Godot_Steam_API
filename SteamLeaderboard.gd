097c62edfa635e51d8d0f473ea9cb8ce5455c695
B1773rm4n <B1773rm4n@asuka-shikinami.club>
2026-05-03 21:45:19 +0700

    Implement upload of an own score

--- SteamLeaderboard.gd
+++ SteamLeaderboard.gd
@@ -28,6 +28,9 @@ func get_single_handle(handle_name: String) -> void:
 	var handle_id = result[0]
 	var success = result[1]
 	
+	# This line does an upload of a score
+	# It is here because it needs the handle and this was the easiest way to get it.
+	# upload_leaderboard_score(2,false,[],handle_id)
 	process_found_leaderboard(handle_id, success, handle_name)
 
 func process_found_leaderboard(handle_id: int, success: int, handle_name: String):
@@ -41,20 +44,34 @@ func process_found_leaderboard(handle_id: int, success: int, handle_name: String
 		var res_handle = result[1]
 		var res_entries = result[2]
 		
-		process_downloaded_leaderboard_scores(res_status,res_handle,res_entries)
+		process_downloaded_leaderboard_scores(res_status, res_handle, res_entries)
 	else:
 		print("Failed to find leaderboard: ", handle_name)
 
 
 func process_downloaded_leaderboard_scores(_res_status: String, _res_handle: int, res_entries: Array) -> void:
-	
 	#print("Downloaded entries res_status: ", res_status)
 	#print("Downloaded entries res_handle: ", res_handle)
 	#print("Downloaded entries res_entries: ", res_entries)
-	
 	for entry in res_entries:
 		var score = entry['score']
 		var rank = entry['global_rank']
 		var steam_id = entry['steam_id']
 		var username = Steam.getFriendPersonaName(steam_id)
 		print("Rank: %d | Name: %s | Score: %d" % [rank, username, score])
+		
+func upload_leaderboard_score(score: int, keep_best: bool, details: Array, this_leaderboard: int) -> void:
+	Steam.uploadLeaderboardScore(score, keep_best, details, this_leaderboard)
+	var result = await Steam.leaderboard_score_uploaded
+	var success = result[0]
+	var res_handle = result[1]
+	var res_score: Dictionary = result[2]
+	
+	print("Uploaded score success: ", success)
+	print("Uploaded score res_handle: ", res_handle)
+	print("Uploaded score res_score (raw): ", res_score)
+	if res_score is Dictionary:
+		print("  score (attempted): ", res_score.score)
+		print("  score_changed: ", res_score.score_changed)
+		print("  global_rank_new: ", res_score.global_rank_new)
+		print("  global_rank_prev: ", res_score.global_rank_prev)
