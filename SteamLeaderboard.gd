028a49ecb1965147962d1ad91b3e0480554c4c0e
B1773rm4n <B1773rm4n@asuka-shikinami.club>
2026-05-03 14:47:57 +0700

    Change Leaderboard to use await - not finished

--- SteamLeaderboard.gd
+++ SteamLeaderboard.gd
@@ -1,62 +1,64 @@
 extends Node
 
 var current_handle: int = 0
-var leaderboard_handles: Dictionary[StringName, int] = {
-	"FeetTraveled": 0,
-	"AverageSpeed": 0,
-	"NumWins": 0
-	}
+
+var leaderboards: Dictionary = {
+	"FeetTraveled": {},
+	"AverageSpeed": {},
+	"NumWins": {}
+}
 
 # Need to connect to LeaderboardScoresDownloaded passing (Rank,Name,Score)
 var LeaderboardUserScore : PackedScene
 
 func _ready()-> void:
-	Steam.leaderboard_find_result.connect(_on_leaderboard_find_result)
-	Steam.leaderboard_score_uploaded.connect(_on_leaderboard_score_uploaded)
-	Steam.leaderboard_scores_downloaded.connect(_on_leaderboard_scores_downloaded)
+	print("=== Start Steam Leaderboard Start ===")
+	get_single_handle("FeetTraveled")
 	
-	Steam.findLeaderboard( leaderboard_handles.keys()[0] )
-	get_handles_in_loop()
-
-
-func _on_leaderboard_find_result(new_handle: int, was_found: int) -> void:
-	if was_found != 1:
-		print("Leaderboard handle could not be found: %s" % was_found)
-		_read_leaderboard_data(current_handle)
-		return
-
-	current_handle = new_handle
-
-	var api_name: String = Steam.getLeaderboardName(new_handle)
-	leaderboard_handles[api_name] = current_handle
-
-	print("Leaderboard %s handle found: %s" % [api_name, current_handle])
-
+# Optional starting point to iterate through all leaderboards
 func get_handles_in_loop() -> void:
-	for this_leaderboard in leaderboard_handles.keys():
-		Steam.findLeaderboard(this_leaderboard)
+	for handle_name in leaderboards:
+		get_single_handle(handle_name)
+	
+# same as get_handles_in_loop but just as a specific single handle
+func get_single_handle(handle_name: String) -> void:
+	print("Finding leaderboard: %s" % handle_name)
+	
+	Steam.findLeaderboard(handle_name)
+	var result = await Steam.leaderboard_find_result
+	var handle_id = result[0]
+	var success = result[1]
+	
+	process_found_leaderboard(handle_id, success, handle_name)
 
-		await Steam.leaderboard_find_result
-		_read_leaderboard_data(current_handle)
+func process_found_leaderboard(handle_id: int, success: int, handle_name: String):
+	if success == 1:
+		# 3. Store the handle_id in the dictionary
+		leaderboards[handle_name]["handle_id"] = handle_id
+		
+		# 4. Request the entries using the retrieved handle_id
+		Steam.downloadLeaderboardEntries(1, 10, Steam.LEADERBOARD_DATA_REQUEST_GLOBAL, handle_id)
+		
+		# 5. Wait for the specific download signal
+		var result = await Steam.leaderboard_scores_downloaded
+		var res_status = result[0]
+		var res_handle = result[1]
+		var res_entries = result[2]
+		
+		process_downloaded_leaderboard_scores(res_status,res_handle,res_entries)
+	else:
+		print("Failed to find leaderboard: ", handle_name)
 
-func _read_leaderboard_data(current_handle) -> void:
-	# Parameters: Range Start, Range End, Request Type
-	# Steam.LEADERBOARD_DATA_REQUEST_GLOBAL fetches the top scores
-	Steam.downloadLeaderboardEntries(1, 10, Steam.LEADERBOARD_DATA_REQUEST_GLOBAL, current_handle)
 
-
-func _on_leaderboard_scores_downloaded(message: String, result: Array) -> void:
-	for entry in result:
+func process_downloaded_leaderboard_scores(_res_status: String, _res_handle: int, res_entries: Array) -> void:
+	
+	#print("Downloaded entries res_status: ", res_status)
+	#print("Downloaded entries res_handle: ", res_handle)
+	#print("Downloaded entries res_entries: ", res_entries)
+	
+	for entry in res_entries:
 		var score = entry['score']
-		var rank = entry['rank']
+		var rank = entry['global_rank']
 		var steam_id = entry['steam_id']
 		var username = Steam.getFriendPersonaName(steam_id)
-		print("Message from Download: " + message)
 		print("Rank: %d | Name: %s | Score: %d" % [rank, username, score])
-
-		
-func _on_leaderboard_score_uploaded(success: int, this_handle: int, this_score: Dictionary) -> void:
-	if success == 0:
-		print("Failed to upload score to leaderboard %s" % this_handle)
-		return
-	print("Successfully uploaded score to leaderboard %s" % this_handle)
