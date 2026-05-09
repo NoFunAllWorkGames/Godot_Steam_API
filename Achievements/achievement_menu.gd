db2ce1ee499231e3bbcdba0e3a5d9be5c5fabf57
B1773rm4n <B1773rm4n@asuka-shikinami.club>
2026-05-09 10:37:23 +0700

    Get all achievements and then iterate through them

--- Achievements/achievement_menu.gd
+++ Achievements/achievement_menu.gd
@@ -2,11 +2,7 @@ extends Control
 
 const achievementPanel: PackedScene = preload("res://Achievements/achievement_panel.tscn")
 
-var achievements: Dictionary[String, bool] = {
-	"ACH_TRAVEL_FAR_ACCUM": false,
-	"ACH_TRAVEL_FAR_SINGLE": false,
-	"ACH_WIN_100_GAMES": false
-	}
+var existing_achievements: Array = []
 
 var statistics: Dictionary[String, int] = {
 	"highscore": 0,
@@ -20,9 +16,32 @@ func _ready() -> void:
 func _on_visibility_changed() -> void:
 	if is_visible_in_tree():
 		print("=== START Steam Achievement Load ====")
+		existing_achievements = get_all_achievements()
 		load_steam_achievements()
 		print("=== END Steam Achievement Load ====")
 
+
+func get_all_achievements() -> Array:
+	var achievement_list: Array = []
+
+	# 1. Get the total number of achievements defined in Steamworks
+	var total_achievements: int = Steam.getNumAchievements()
+
+	# 2. Iterate through each index to get the API Name
+	for i in range(total_achievements):
+		var achievement_name: String = Steam.getAchievementName(i)
+
+		# 3. Retrieve specific data for that achievement
+		var achievement_data: Dictionary = Steam.getAchievement(achievement_name)
+
+		# 'ret' indicates if the call was successful
+		if achievement_data['ret']:
+			achievement_list.append({
+				"achievement_name": achievement_name,
+				"is_unlocked": achievement_data['achieved']
+			})
+
+	return achievement_list
 
 # Process achievements
 	# Does the achievement actually exist in the Steamworks back-end?
@@ -32,29 +51,20 @@ func _on_visibility_changed() -> void:
 	# https://godotsteam.com/classes/user_stats/?h=achiev#getachievement
 	# See Returns: dictionary -> Contains the following keys: -> ret
 func load_steam_achievements() -> void:
-	for this_achievement in achievements.keys():
-		var steam_achievement: Dictionary = Steam.getAchievement(this_achievement)
-		
-
-		if not steam_achievement['ret']:
-			print("Steam does not have this achievement, ignoring it")
-			continue
-
-		achievements[this_achievement] = steam_achievement['achieved']
-
-	for key in achievements:
-		var achievement_status = achievements[key]
+	for this_achievement in existing_achievements:
+		var achievement_name = this_achievement["achievement_name"]
+		var is_unlocked = this_achievement["is_unlocked"]
 		var grid = achievementPanel.instantiate()
 		$Panel/MarginContainer/ScrollContainer/GridContainer.add_child(grid)
-		grid.get_node("MarginContainer/HBoxContainer/VBoxContainer/Name").text = str("Key: %s" % key)
-		grid.get_node("MarginContainer/HBoxContainer/VBoxContainer/Status").text = str("Unlocked?: %s" % achievement_status)
-		var icon = grid.get_node("MarginContainer/HBoxContainer/TextureRect")
-		load_achievement_icon(icon, key)
-		
-		print("Key: %s " % key, "Unlocked?: %s " % achievement_status)
+		grid.get_node("MarginContainer/HBoxContainer/VBoxContainer/Name").text = str("Key: %s" % achievement_name)
+		grid.get_node("MarginContainer/HBoxContainer/VBoxContainer/Status").text = str("Unlocked?: %s" % is_unlocked)
+		var iconRect = grid.get_node("MarginContainer/HBoxContainer/TextureRect")
+		load_achievement_icon(iconRect, achievement_name)
 
-func load_achievement_icon(icon: TextureRect, k) -> void:
-	var icon_handle: int = Steam.getAchievementIcon(k)
+		print("Key: %s " % achievement_name, "Unlocked?: %s " % is_unlocked)
+
+func load_achievement_icon(iconRect: TextureRect, achievement_name: String) -> void:
+	var icon_handle: int = Steam.getAchievementIcon(achievement_name)
 
 	var icon_size: Dictionary = Steam.getImageSize(icon_handle)
 	var icon_buffer: Dictionary = Steam.getImageRGBA(icon_handle)
@@ -64,4 +74,4 @@ func load_achievement_icon(icon: TextureRect, k) -> void:
 	var icon_image: Image = Image.create_from_data(icon_size.width, icon_size.height, false, Image.FORMAT_RGBA8, icon_buffer["buffer"])
 
 	var icon_texture: ImageTexture = ImageTexture.create_from_image(icon_image)
-	icon.texture = icon_texture
+	iconRect.texture = icon_texture
